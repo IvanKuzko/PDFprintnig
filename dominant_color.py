fee89460e9f2a32b5bb42c53b2af794ea10efc12
IvanKuzko <ivankuzko@seznam.cz>
2022-07-12 16:59:00 +0200

    Added dominant color

---
+++ dominant_color.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+import numpy as np
+import scipy.cluster
+
+def get_dominant(pil):
+    NUM_CLUSTERS = 5
+
+    im = pil
+    im = im.resize((150, 150))      # optional, to reduce time
+    ar = np.asarray(im)
+    shape = ar.shape
+    ar = ar.reshape(scipy.product(shape[:2]), shape[2]).astype(float)
+
+    codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
+
+    vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
+    counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
+
+    index_max = scipy.argmax(counts)                    # find most frequent
+    peak = codes[index_max]
+    return tuple(peak)
+
